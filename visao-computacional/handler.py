89782eadf926fccd66bf9baa081319306f148db2
https-Luan-Fernandes <jose.luan@academico.ifpb.edu.br>
2024-08-15 01:11:46 -0300

    :sparkles: feat: implementar função face_emotion_analysis_v1 com lógica de placeholder
    - Adicionada estrutura básica para a função face_emotion_analysis_v1.
    - Manipula entrada JSON e formata resposta para casos com e sem faces detectadas.
    - Inclui tratamento de erros com status code e mensagem apropriados.

--- visao-computacional/handler.py
+++ visao-computacional/handler.py
@@ -28,3 +28,56 @@ def v2_description(event, context):
     response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
+
+def face_emotion_analysis_v1(event, context):
+    try:
+        body = json.loads(event['body'])
+        bucket = body.get('bucket')
+        image_name = body.get('imageName')
+
+        # Adicionar lógica para chamada ao Rekognition e processamento da resposta;
+        # obter a lista de faces detectadas; lógica para processar o Rekognition e preencher a lista `faces.
+
+        faces = [] 
+
+
+        if not faces:
+            response_body = {
+                "url_to_image": f"https://{bucket}/{image_name}",
+                "created_image": "02-02-2023 17:00:00",
+                "faces": [
+                    {
+                        "position": {
+                            "Height": None, # não alterar parar null
+                            "Left": None,   # a função dumps converte
+                            "Top": None,
+                            "Width": None
+                        },
+                        "classified_emotion": None,
+                        "classified_emotion_confidence": None
+                    }
+                ]
+            }
+        else:
+            response_body = {
+                "url_to_image": f"https://{bucket}/{image_name}",
+                "created_image": "02-02-2023 17:00:00",
+                "faces": faces  # lista de faces detectadas
+            }
+
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(response_body)
+        }
+    except Exception as e:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps(
+                {
+                    "message": "Erro ao processar a solicitação.",
+                    "error": str(e)
+                }
+            )
+        }
+
+    return response
